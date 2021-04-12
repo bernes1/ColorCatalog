f6920f947f0073cb2bf3accb66806ca6cae0d470
ducky <bremnes14@gmail.com>
2021-04-12 16:30:33 +0200

    did it ()

--- App.js
+++ App.js
@@ -1,45 +1,46 @@
-import React, { useState } from "react"
-import { StyleSheet, FlatList, } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, FlatList, Alert } from "react-native";
 import ColorButton from "./components/ColorButton";
 import ColorForm from "./components/ColorForm";
 import { generate } from "shortid";
 
+const useColors = () => {
+  const [colors, setColors] = useState([]);
+  const addColor = color => {
+    const newColor = { id: generate(), color };
+    setColors([newColor, ...colors]);
+  };
+  return { colors, addColor };
+};
 
-export default function App(){
- const  [backgroundColor, setBackgroundColor] = useState("brown");
- const  [colors, setColors] = useState([]);
- const  addColor = color =>{
-   const newColor = { id: generate(), color }
-   setColors
- }
-
+export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState(
+    "brown"
+  );
+  const { colors, addColor } = useColors();
   return (
-   <> 
-    <ColorForm
-      onNewColor={newColor => Alert.alert(`TODO: add color ${newColor}`)}
-    />
-    <FlatList 
-    style={[styles.container, { backgroundColor}]} 
-    data={colors}
-    renderItem={({ item }) =>{
-      return(
-        <ColorButton 
-          key={item.id} 
-          backgroundColor={item.color} 
-          onPress={setBackgroundColor} 
-          />
-      )
-     }}
-    />
-  </>
+    <>
+      <ColorForm onNewColor={addColor} />
+      <FlatList
+        style={[styles.container, { backgroundColor }]}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <ColorButton
+              key={item.id}
+              backgroundColor={item.color}
+              onPress={setBackgroundColor}
+            />
+          );
+        }}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
-    display: "flex",
-  },
-  
+    display: "flex"
+  }
 });
